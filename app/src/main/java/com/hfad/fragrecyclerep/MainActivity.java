219bba0bb768126afc1ee3628ed9df91cf7e695a
aleks-hd2015 <aleks-hd2015@yandex.ru>
2021-07-14 21:38:56 +0400

    Добавили обработчик событий на меню

--- app/src/main/java/com/hfad/fragrecyclerep/MainActivity.java
+++ app/src/main/java/com/hfad/fragrecyclerep/MainActivity.java
@@ -1,10 +1,13 @@
 package com.hfad.fragrecyclerep;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,4 +31,34 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onCreateOptionsMenu(menu);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.openCard:
+                openNodes();
+                return true;
+            case R.id.addCard:
+                Log.d("LOGIII", "addCard !!!");
+                return true;
+            case R.id.editCard:
+                Log.d("LOGIII", "editCard !!!");
+                return true;
+            case R.id.deleteCard:
+                Log.d("LOGIII", "deleteCard !!!");
+                return true;
+            case R.id.suprizeCard:
+                Log.d("LOGIII", "aboutProgramm !!!");
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+   void openNodes(){
+       Log.d("LOGIII", "OpenCard !!!");
+       
+
+    }
+
 }
